4d3528e56799fb70fda28ffb218bea80ba677e8f
shadman96 <mdsadman96@gmail.com>
2019-09-16 11:44:21 +0530

    code clean

--- screens/scan.js
+++ screens/scan.js
@@ -11,7 +11,7 @@ import {
 
 
 const Scan = (props) => {
-    const { camera, cameraResult, clickAgain, takePicture, activeCamera, googleVisionDetetion, loading, sendSMS, getSMS, deleteSMS } = props
+    const { sendSMS, getSMS, deleteSMS } = props
     const desccription = 'Use this RN component to send an SMS with a callback (completed/cancelled/error). iOS and Android are both supported.Currently, only user-initiated sending of an SMS is supported. This means you can\'t use react-native-sms to send an SMS in the background-- this package displays the native SMS view (populated with any recipients/body you want), and gives a callback describing the status of the SMS (completed/cancelled/error)'
     const imageUri = 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/sms.png';
     return (
